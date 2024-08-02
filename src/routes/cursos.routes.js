cda539aead043ca74f49589b309f20dd100638f1
Rodrigo Eduardo <rodrigo_e_soares@estudante.sesisenai.org.br>
2024-08-02 20:49:46 -0300

    exercício 04

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -6,6 +6,7 @@ const cursosRoutes = new Router()
 
 cursosRoutes.post('/', CursoController.criar)
 cursosRoutes.get('/', CursoController.listaTodos)
+cursosRoutes.put('/:id', CursoController.atualizar)
 
 
 module.exports = cursosRoutes
